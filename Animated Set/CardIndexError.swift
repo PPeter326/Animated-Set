f27954aecebbbfdff0dab724797ef00bf51f80f9
Peter Wu <petey888@gmail.com>
2018-06-14 18:40:28 -0700

    Added the following animations to set game: - cards move gradually when frames are updated - when dealt, cards appear gradually - when cards are matched and taken away, replacement cards appeared gradually

---
+++ Animated Set/CardIndexError.swift
@@ -0,0 +1,14 @@
+//
+//  CardIndexError.swift
+//  Animated Set
+//
+//  Created by Peter Wu on 6/14/18.
+//  Copyright © 2018 Zero. All rights reserved.
+//
+
+import Foundation
+
+enum CardIndexError: Error {
+    
+    case invalidIndex
+}
